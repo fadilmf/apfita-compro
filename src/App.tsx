95616c976e044a359ec94eab8c4bebdc05e2868c
Surya Fatihah Helmianto <95084814+suryafatihahhelmianto@users.noreply.github.com>
2025-02-18 12:40:31 +0700

    ngide
    hapus ajah ea

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "@/routes/AppRoutes";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import MobileSidebar from "@/components/MobileSidebar";
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
-      <Footer />
-      <Navbar />
-      <MobileSidebar />
     </BrowserRouter>
   );
 }
