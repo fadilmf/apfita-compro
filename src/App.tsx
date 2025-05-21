2f64c5fa8715f6f412d7366a7fab11810d484b96
Surya Fatihah Helmianto <95084814+suryafatihahhelmianto@users.noreply.github.com>
2025-05-21 13:18:40 +0700

    hajar!
    hajar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { BrowserRouter, useLocation } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "@/routes/AppRoutes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Sidebar from "@/components/MobileSidebar";
-import { useState } from "react";
 
-function LayoutWrapper() {
-  const location = useLocation();
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  // Halaman yang tidak pakai layout
-  const noLayoutRoutes = ["/selamat-ya-mas-pian"];
-  const hideLayout = noLayoutRoutes.includes(location.pathname);
-
-  return (
-    <>
-      {!hideLayout && <Navbar />}
-      <AppRoutes />
-      {!hideLayout && <Footer />}
-      {!hideLayout && (
-        <Sidebar isOpen={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
-      )}
-    </>
-  );
-}
-
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
-      <LayoutWrapper />
+      <AppRoutes />
+      <Navbar />
+      <Footer />
+      <Sidebar
+        isOpen={false}
+        onClose={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </BrowserRouter>
   );
 }
+
+export default App;
