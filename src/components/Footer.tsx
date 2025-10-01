1fa08953d028496d48820a5b48f76ed745a9ad94
Surya Fatihah Helmianto <95084814+suryafatihahhelmianto@users.noreply.github.com>
2025-10-01 13:25:00 +0700

    Menghilangkan Nomer Mba Efri
    Mba Efri sudah vakum:(

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -82,8 +82,7 @@ const emailClients = [
 const phoneNumbers = [
   { name: "General Support", number: "+62-852-8242-5228" },
   { name: "Technical Support", number: "+62 822-1426-9503" },
-  { name: "Admin Support1", number: "+62-812-7451-3242" },
-  { name: "Admin Support2", number: "+62 812-8257-5650" },
+  { name: "Admin Support", number: "+62 812-8257-5650" },
 ];
 
 const navigation = [
